e8ee345c89244bdd831552ffdbc557fd16db1f5f
Nathan Pillai <natepill@gmail.com>
2019-09-09 17:34:00 -0700

    dkfjdkf

--- app.py
+++ app.py
@@ -31,15 +31,15 @@ def index():
 def test_request():
     parser = argparse.ArgumentParser()
     parser.add_argument("diet")
-    parser.add_argument("timeline", type =list)
+    parser.add_argument("timeline", type=list)
     args = parser.parse_args()
+
+    print(args.diet, args.timeline)
+
 
     data = requests.get('http://127.0.0.1:5000/')
 
-
-
-
-
+    print('DATA RETRIVED: {}'.format(data))
 
 
 
